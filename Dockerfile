59bc945c4093904cc10f2929f9250111286113fc
amarouane <am0089@uah.edu>
2019-02-23 19:45:51 -0600

    Adding dist permission

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,9 @@ RUN apt-get update && \
 RUN useradd -u 500 -ms /bin/bash bamboo
 USER bamboo 
 COPY build.sh /home/bamboo
-WORKDIR /home/bamboo/data
-ENTRYPOINT [ "/bin/bash", "/home/bamboo/build.sh" ]
+ENV HOME=/home/bamboo
+RUN mkdir $HOME/process $HOME/dist && \
+    chmod 777 $HOME/dist
+WORKDIR /home/bamboo/process
+ENTRYPOINT [ "/bin/bash", "/home/bamboo/build.sh"] ]
 
--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
+cp -r $HOME/data $HOME/process/.
+cd data
 pip install --user -r requirements.txt
 cd /home/bamboo/.local/lib/python3.7/site-packages
 zip -r9 pyfunction.zip .
-mv pyfunction.zip /home/bamboo/data/.
-cd /home/bamboo/data/
+mv pyfunction.zip /home/bamboo/process/data/.
+cd /home/bamboo/process/data/
 zip -g pyfunction.zip handler.py
 zip -ur pyfunction.zip src
 mv pyfunction.zip /home/bamboo/dist/.
