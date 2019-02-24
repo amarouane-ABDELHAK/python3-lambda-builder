d752786b7c422acaeb1a2bf396e5f4ee36037771
amarouane <am0089@uah.edu>
2019-02-23 23:19:08 -0600

    Adding dist permission

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 cp -r $HOME/data $HOME/process/.
 cd data
-pip install --user -r requirements.txt
-cp -r src /home/bamboo/.local/lib/python3.7/site-packages/.
-cd /home/bamboo/.local/lib/python3.7/site-packages
-zip -r9 pyfunction.zip .
-mv pyfunction.zip /home/bamboo/process/data/.
-cd /home/bamboo/process/data/
+mkdir package
+cd package
+pip install -r ../requirements.txt --target .
+cp -r ../src .
+zip -r9 ../pyfunction.zip .
+cd ../
 zip -g pyfunction.zip handler.py
 mv pyfunction.zip /home/bamboo/dist/.
